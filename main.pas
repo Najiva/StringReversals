b683933cfa4e72f96b7bda3c38fa1df8a0f3e28e
Juraj Skvarla <jurajskvarla@yahoo.com>
2017-03-21 13:01:40 +0100

    circle queue + BFS not working yet

--- main.pas
+++ main.pas
@@ -8,17 +8,22 @@ Type
 				children: array[1..10] of ^Node;
 				Constructor Init;
 				Destructor Done;
+
+				//function isLeaf(): boolean;
 		   End;
 
 // global variabless
 const
-  qLength = 10; // maximal queue length
+  MAX = 10; // maximal queue length
+	STACK_SIZE = 10;
 Var
 ip, tp: String;
 N, i, j, k: Integer;
 root, root1: ^Node;
-NodesQueue : array[1..qLength] of ^Node;
-HeadIndex, RearIndex: Integer;
+test: Node;
+NodesQueue : array[1..MAX] of Node;
+// for queue
+HeadIndex, RearIndex, QueueLength: Integer;
 
 Constructor Node.Init;
 begin
@@ -29,38 +34,45 @@ Destructor Node.Done;
 begin
   Writeln ('Destroying an instance of Node !');
 end;
+
+{function Node.isLeaf(): boolean;
+begin
+	if perm.
+end;
+}
+	// http://www.pp4s.co.uk/main/tu-lsq-cqueues-demo.html
 	// --------------- queue ---------------------
 
-procedure Enqueue(var LastNode: Node);
-begin
-  if RearIndex < qLength then
-    begin
-      inc(RearIndex);
-			writeln('Enqueing node with perm: ', LastNode.perm);
-      NodesQueue[RearIndex] := @LastNode;
-    end
-  else
-    writeln('Queue full')
-end;
 
-function Dequeue : Node;
-begin
-	  if HeadIndex <= RearIndex then
+	procedure Enqueue(LastNode: Node);
+	begin
+	  if QueueLength < MAX then
 	    begin
-				writeln('Dequeing node with perm: ', NodesQueue[HeadIndex]^.perm);
-	      Dequeue := NodesQueue[HeadIndex]^;
-	      inc(HeadIndex);
+	      RearIndex := (RearIndex MOD MAX) + 1;
+	      NodesQueue[RearIndex]:= LastNode;
+	      inc(QueueLength);
 	    end
 	  else
-			begin
-			writeln('Queue is empty');
-			end;
-end;
+	    writeln('Queue full')
+	end;
+
+	function Dequeue : Node;
+	begin
+	  if QueueLength = 0 then
+	    writeln('Queue is empty')
+	  else
+	    begin
+	      Dequeue := NodesQueue[HeadIndex];
+	      HeadIndex := (HeadIndex MOD MAX) + 1;
+	      dec(QueueLength);
+	    end;
+	end;
 
 procedure InitQueue;
 begin
-   HeadIndex := 1;
-   RearIndex := 0;
+	HeadIndex := 1;
+	RearIndex := MAX;
+	QueueLength := 0;
 end;
 
 // procedura otoci substring v retazci y zacinajuci na indexe A a konciaci na indexe B
@@ -114,6 +126,36 @@ Begin
 	End;
 End;
 
+function BFS(root: Node; goal: Node): boolean;
+Var
+children: array[1..10] of ^Node;
+i: integer;
+t: Node;
+begin
+
+	Enqueue(root);
+
+	while(QueueLength<>0) do
+	begin
+		t := NodesQueue[HeadIndex];
+		//path += t.getValue();
+
+		Dequeue();
+
+		if(t.perm = goal.perm) then
+			begin
+			exit(TRUE);
+			end;
+		//children = t.getChildren();
+		for i := 0 to 10 do
+		begin
+			Enqueue(root.children[i]^);
+		end;
+	end;
+	BFS:=TRUE;
+end;
+
+
 
 Begin
 // length of the permutation
@@ -128,15 +170,19 @@ ip := '34321';
 writeln('Target permutation: ', tp);
 writeln('Initial permutation: ', ip);
 
+test.perm:='55555';
 New(root, Init);
 root^.perm:=ip;
 New(root1, Init);
 root1^.perm:=tp;
-Enqueue(root1^);
+
+Enqueue(test);
 Enqueue(root^);
-Enqueue(root^);
+writeln('Head: ', NodesQueue[HeadIndex].perm);
+writeln('Queuelength: ', QueueLength);
 Dequeue();
-Enqueue(root1^);
+writeln('Head: ', NodesQueue[HeadIndex].perm);
+writeln('Queuelength: ', QueueLength);
 
 
 {j := 1;
