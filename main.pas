e4ba179453d11eb343f44bf9e5097de91f669a27
Juraj Skvarla <jurajskvarla@yahoo.com>
2017-03-21 11:29:05 +0100

    added queue for BFS

--- main.pas
+++ main.pas
@@ -2,7 +2,7 @@ Program missionX;
 
 Type
 	iPerm    = String[5];
-	// class Node
+	// ---------------class Node-------------------
 	Node = Object
 				perm : iPerm;
 				children: array[1..10] of ^Node;
@@ -11,21 +11,56 @@ Type
 		   End;
 
 // global variabless
+const
+  qLength = 10; // maximal queue length
 Var
-fp, tp: String;
+ip, tp: String;
 N, i, j, k: Integer;
-root: ^Node;
+root, root1: ^Node;
+NodesQueue : array[1..qLength] of ^Node;
+HeadIndex, RearIndex: Integer;
 
 Constructor Node.Init;
-
 begin
  Writeln ('Initializing an instance of Node !');
 end;
 
 Destructor Node.Done;
-
 begin
   Writeln ('Destroying an instance of Node !');
+end;
+	// --------------- queue ---------------------
+
+procedure Enqueue(var LastNode: Node);
+begin
+  if RearIndex < qLength then
+    begin
+      inc(RearIndex);
+			writeln('Enqueing node with perm: ', LastNode.perm);
+      NodesQueue[RearIndex] := @LastNode;
+    end
+  else
+    writeln('Queue full')
+end;
+
+function Dequeue : Node;
+begin
+	  if HeadIndex <= RearIndex then
+	    begin
+				writeln('Dequeing node with perm: ', NodesQueue[HeadIndex]^.perm);
+	      Dequeue := NodesQueue[HeadIndex]^;
+	      inc(HeadIndex);
+	    end
+	  else
+			begin
+			writeln('Queue is empty');
+			end;
+end;
+
+procedure InitQueue;
+begin
+   HeadIndex := 1;
+   RearIndex := 0;
 end;
 
 // procedura otoci substring v retazci y zacinajuci na indexe A a konciaci na indexe B
@@ -34,7 +69,7 @@ Var
 max: Integer;
 temp: Char;
 Begin
-	if Length(y)< B then 
+	if Length(y)< B then
 		Begin
 		writeln('Index out of bounds! Exiting procedure swap.');
 		exit();
@@ -59,9 +94,10 @@ Begin
 	for il := 1 to N-1 do
 		Begin
 			New(temp, Init);
-			swap(il,il+1,fp);
-			temp^.perm := fp;
-			swap(il,il+1,fp);
+			temp^.perm := node.perm;
+			swap(il,il+1,temp^.perm);
+			writeln('Creating child node with permutation: ', temp^.perm);
+			//swap(il,il+1,node.perm);
 			node.children[il] := temp;
 		End;
 End;
@@ -70,9 +106,9 @@ Procedure printTree(root: Node);
 Var
 i: Integer;
 Begin
-	for i :=1 to 10 do 
+	for i :=1 to 10 do
 	Begin
-		writeln(root.perm);
+		writeln(i, ': ' ,root.perm);
 		if root.children[i]<>Nil then
 		printTree(root.children[i]^);
 	End;
@@ -82,23 +118,36 @@ End;
 Begin
 // length of the permutation
 N := 5;
+InitQueue;
 
 // example permutations
 tp := '12334';
-fp := '34321';
+ip := '34321';
+
+
+writeln('Target permutation: ', tp);
+writeln('Initial permutation: ', ip);
 
 New(root, Init);
-root^.perm:=fp;
+root^.perm:=ip;
+New(root1, Init);
+root1^.perm:=tp;
+Enqueue(root1^);
+Enqueue(root^);
+Enqueue(root^);
+Dequeue();
+Enqueue(root1^);
 
-j := 1;
+
+{j := 1;
 k := 5;
 
 createChildrenNodes(root^);
 printTree(root^);
 
 
-swap(j,k,fp);
-writeln(fp);
+swap(j,k,ip);
+writeln(ip);}
 
 
 End.
